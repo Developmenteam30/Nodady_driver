c4fd3a748766e67b2be26ce360933f3b88fa7fba
Shikhar Agarwal <shikharagarwal2807@gmail.com>
2022-03-23 21:06:43 +0530

    barcode scanner fix

--- src/Routes/SpecificDelivery/index.jsx
+++ src/Routes/SpecificDelivery/index.jsx
@@ -21,6 +21,7 @@ const SpecificDelivery = () => {
   const notification = useContext(NotificationContext);
   const navigate = useNavigate();
   const [buttons, setButtons] = useState([]);
+  const [apiCallMade, setApiCallMade] = useState(false);
 
   useEffect(() => {
     getPickups();
@@ -40,11 +41,19 @@ const SpecificDelivery = () => {
       );
       if (res?.data) {
         setButtons(res.data.detail.button);
+        setApiCallMade(true);
         session.setForwardOrderDetails(res.data.detail);
         session.setIsLoading(false);
       }
     } catch (error) {
       session.setIsLoading(false);
+      if (error.response.data.detail) {
+        navigate(-1);
+        return notification.setNotificationObject({
+          type: 'error',
+          message: error.response.data.detail,
+        });
+      }
       if (error.response) {
         let firstError =
           Object.values(error.response.data) &&
@@ -111,210 +120,214 @@ const SpecificDelivery = () => {
 
   return (
     <>
-      <StatusBar backgroundColor="white" barStyle="dark-content" />
-      <BreadCrumbs
-        title={`Order ID: #${session.forwardOrderDetails.order_id}`}
-      />
-      <ScrollView keyboardShouldPersistTaps="handled">
-        <View style={Styles.container}>
-          <View style={Styles.cardSection}>
-            <View style={Styles.card}>
-              <Image source={LocationIcon} />
-              <Text style={{ ...Styles.subHeading, marginTop: 30 }}>
-                {session.forwardOrderDetails.delivery_state}
-              </Text>
-              <Text style={Styles.subText}>
-                {session.forwardOrderDetails.delivery_pincode}
-              </Text>
-            </View>
-            <View style={Styles.card}>
-              <Image source={CodIcon} />
-              <Text style={Styles.subHeading}>
-                {session.forwardOrderDetails?.payment_mode?.toUpperCase()}
-              </Text>
-              <Text style={Styles.subText}></Text>
-              <View style={{ marginTop: 15 }} />
-              <View style={{ marginBottom: 15 }} />
-              <Text style={Styles.subHeading}>Order Value</Text>
-              <Text style={{ ...Styles.subHeading, marginTop: 0 }}>
-                ₹ {session.forwardOrderDetails?.order_price}
-              </Text>
-            </View>
-            <View style={Styles.card}>
-              <Image source={WeightIcon} />
-              <Text style={Styles.subHeading}>Applicable{'\n'} Weight</Text>
-              <View style={{ marginTop: 15 }} />
-              <View style={{ marginBottom: 15 }} />
-              <Text style={Styles.subHeading}>
-                {session.forwardOrderDetails.product_weight} KG
-              </Text>
-              <Text style={Styles.subText}>Entered Weight</Text>
-            </View>
-          </View>
-          <View style={Styles.shipmentSection}>
-            <Text style={Styles.heading}>Shipment Details</Text>
-            <View style={Styles.rowSection}>
-              <View>
-                <Text style={Styles.subText}>Pickup Address</Text>
-                <Text style={Styles.subTitle}>
-                  {session.forwardOrderDetails.pickup_address_line_1}{' '}
-                  {session.forwardOrderDetails.pickup_address_line_2}
-                </Text>
-              </View>
-            </View>
-            <View style={{ ...Styles.rowSection, marginTop: 16 }}>
-              <View>
-                <Text style={Styles.subText}>Delivery Address</Text>
-                <Text style={Styles.subTitle}>
-                  {session.forwardOrderDetails.delivery_address_line_1}{' '}
-                  {session.forwardOrderDetails.delivery_address_line_2}
-                </Text>
-              </View>
-            </View>
-            <View
-              style={{
-                ...Styles.rowSection,
-                marginTop: 16,
-              }}>
-              <View style={{ width: '50%' }}>
-                <Text style={Styles.subText}>Customer Name</Text>
-                <Text style={Styles.subTitle}>
-                  {session.forwardOrderDetails.customer_first_name}{' '}
-                  {session.forwardOrderDetails.customer_last_name}
-                </Text>
-              </View>
-              <View style={{ width: '50%' }}>
-                <Text style={Styles.subText}>Reseller Name</Text>
-                <Text style={Styles.subTitle}>
-                  {session.forwardOrderDetails.reseller_name}
-                </Text>
-              </View>
-            </View>
-            <View
-              style={{
-                ...Styles.rowSection,
-                marginVertical: 16,
-              }}>
-              <View style={{ width: '50%' }}>
-                <Text style={Styles.subText}>Customer Email</Text>
-                <Text style={Styles.subTitle}>
-                  {session.forwardOrderDetails.customer_email}
-                </Text>
-              </View>
-              <View style={{ width: '50%' }}>
-                <Text style={Styles.subText}>Customer Phone</Text>
-                <Text style={Styles.subTitle}>
-                  {session.forwardOrderDetails.customer_phone_number}
-                </Text>
-              </View>
-            </View>
-          </View>
-          <View style={Styles.shipmentSection}>
-            <Text style={Styles.heading}>Order Details</Text>
-            <View style={Styles.rowSection}>
-              <View
-                style={{
-                  ...Styles.rowSection,
-                  justifyContent: 'space-between',
-                  width: '90%',
-                }}>
-                <View style={{ width: '45%' }}>
-                  <Text style={Styles.subText}>Order ID</Text>
-                  <Text style={Styles.subTitle}>
-                    {session.forwardOrderDetails.order_id}
+      {apiCallMade && (
+        <>
+          <StatusBar backgroundColor="white" barStyle="dark-content" />
+          <BreadCrumbs
+            title={`Order ID: #${session.forwardOrderDetails.order_id}`}
+          />
+          <ScrollView keyboardShouldPersistTaps="handled">
+            <View style={Styles.container}>
+              <View style={Styles.cardSection}>
+                <View style={Styles.card}>
+                  <Image source={LocationIcon} />
+                  <Text style={{ ...Styles.subHeading, marginTop: 30 }}>
+                    {session.forwardOrderDetails.delivery_state}
+                  </Text>
+                  <Text style={Styles.subText}>
+                    {session.forwardOrderDetails.delivery_pincode}
                   </Text>
                 </View>
-                <View style={{ width: '45%' }}>
-                  <Text style={Styles.subText}>Payment Method</Text>
-                  <Text style={Styles.subTitle}>
+                <View style={Styles.card}>
+                  <Image source={CodIcon} />
+                  <Text style={Styles.subHeading}>
                     {session.forwardOrderDetails?.payment_mode?.toUpperCase()}
                   </Text>
-                </View>
-              </View>
-            </View>
-            <View
-              style={{
-                ...Styles.rowSection,
-                marginTop: 16,
-              }}>
-              <View style={{ width: '50%' }}>
-                <Text style={Styles.subText}>Order Created on</Text>
-                <Text style={Styles.subTitle}>
-                  {new Date(
-                    session.forwardOrderDetails.created_at,
-                  ).toLocaleDateString()}
-                </Text>
-              </View>
-              <View style={{ width: '50%' }}>
-                <Text style={Styles.subText}>Total Product Price</Text>
-                <Text style={Styles.subTitle}>
-                  Rs. {session.forwardOrderDetails.order_price}
-                </Text>
-              </View>
-            </View>
-            <View style={Styles.rowSection}>
-              <View
-                style={{
-                  ...Styles.rowSection,
-                  marginTop: 16,
-                  width: '90%',
-                }}>
-                <View style={{ width: '45%' }}>
-                  <Text style={Styles.subText}>SKU</Text>
-                  <Text style={Styles.subTitle}>
-                    {session.forwardOrderDetails.sku || '-'}
+                  <Text style={Styles.subText}></Text>
+                  <View style={{ marginTop: 15 }} />
+                  <View style={{ marginBottom: 15 }} />
+                  <Text style={Styles.subHeading}>Order Value</Text>
+                  <Text style={{ ...Styles.subHeading, marginTop: 0 }}>
+                    ₹ {session.forwardOrderDetails?.order_price}
                   </Text>
                 </View>
-                <View style={{ width: '45%' }}>
-                  <Text style={Styles.subText}>HSN</Text>
-                  <Text style={Styles.subTitle}>
-                    {session.forwardOrderDetails.hsn || '-'}
+                <View style={Styles.card}>
+                  <Image source={WeightIcon} />
+                  <Text style={Styles.subHeading}>Applicable{'\n'} Weight</Text>
+                  <View style={{ marginTop: 15 }} />
+                  <View style={{ marginBottom: 15 }} />
+                  <Text style={Styles.subHeading}>
+                    {session.forwardOrderDetails.product_weight} KG
                   </Text>
+                  <Text style={Styles.subText}>Entered Weight</Text>
                 </View>
               </View>
-            </View>
-            <View
-              style={{
-                ...Styles.rowSection,
-                marginVertical: 16,
-                width: '90%',
-              }}>
-              <View style={{ width: '45%' }}>
-                <Text style={Styles.subText}>Unit Price</Text>
-                <Text style={Styles.subTitle}>
-                  Rs. {session.forwardOrderDetails.product_price}
-                </Text>
-              </View>
-              <View style={{ width: '45%' }}>
-                <Text style={Styles.subText}>Quantity</Text>
-                <Text style={Styles.subTitle}>
-                  {session.forwardOrderDetails.quantity}
-                </Text>
-              </View>
-            </View>
-            <View style={Styles.allButtonSection}>
-              {buttons.map((obj, i) => {
-                return (
-                  <View key={i} style={Styles.editButtonSection}>
-                    <Button
-                      text={obj.name}
-                      onPress={() => {
-                        handleUpdate(obj.post_value);
-                      }}
-                      color={obj.status ? 'white' : Constants.primaryColor}
-                      backgroundColor={
-                        obj.status ? Constants.primaryColor : 'white'
-                      }
-                      borderColor={Constants.primaryColor}
-                      borderWidth={1}
-                    />
+              <View style={Styles.shipmentSection}>
+                <Text style={Styles.heading}>Shipment Details</Text>
+                <View style={Styles.rowSection}>
+                  <View>
+                    <Text style={Styles.subText}>Pickup Address</Text>
+                    <Text style={Styles.subTitle}>
+                      {session.forwardOrderDetails.pickup_address_line_1}{' '}
+                      {session.forwardOrderDetails.pickup_address_line_2}
+                    </Text>
                   </View>
-                );
-              })}
+                </View>
+                <View style={{ ...Styles.rowSection, marginTop: 16 }}>
+                  <View>
+                    <Text style={Styles.subText}>Delivery Address</Text>
+                    <Text style={Styles.subTitle}>
+                      {session.forwardOrderDetails.delivery_address_line_1}{' '}
+                      {session.forwardOrderDetails.delivery_address_line_2}
+                    </Text>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    ...Styles.rowSection,
+                    marginTop: 16,
+                  }}>
+                  <View style={{ width: '50%' }}>
+                    <Text style={Styles.subText}>Customer Name</Text>
+                    <Text style={Styles.subTitle}>
+                      {session.forwardOrderDetails.customer_first_name}{' '}
+                      {session.forwardOrderDetails.customer_last_name}
+                    </Text>
+                  </View>
+                  <View style={{ width: '50%' }}>
+                    <Text style={Styles.subText}>Reseller Name</Text>
+                    <Text style={Styles.subTitle}>
+                      {session.forwardOrderDetails.reseller_name}
+                    </Text>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    ...Styles.rowSection,
+                    marginVertical: 16,
+                  }}>
+                  <View style={{ width: '50%' }}>
+                    <Text style={Styles.subText}>Customer Email</Text>
+                    <Text style={Styles.subTitle}>
+                      {session.forwardOrderDetails.customer_email}
+                    </Text>
+                  </View>
+                  <View style={{ width: '50%' }}>
+                    <Text style={Styles.subText}>Customer Phone</Text>
+                    <Text style={Styles.subTitle}>
+                      {session.forwardOrderDetails.customer_phone_number}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+              <View style={Styles.shipmentSection}>
+                <Text style={Styles.heading}>Order Details</Text>
+                <View style={Styles.rowSection}>
+                  <View
+                    style={{
+                      ...Styles.rowSection,
+                      justifyContent: 'space-between',
+                      width: '90%',
+                    }}>
+                    <View style={{ width: '45%' }}>
+                      <Text style={Styles.subText}>Order ID</Text>
+                      <Text style={Styles.subTitle}>
+                        {session.forwardOrderDetails.order_id}
+                      </Text>
+                    </View>
+                    <View style={{ width: '45%' }}>
+                      <Text style={Styles.subText}>Payment Method</Text>
+                      <Text style={Styles.subTitle}>
+                        {session.forwardOrderDetails?.payment_mode?.toUpperCase()}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    ...Styles.rowSection,
+                    marginTop: 16,
+                  }}>
+                  <View style={{ width: '50%' }}>
+                    <Text style={Styles.subText}>Order Created on</Text>
+                    <Text style={Styles.subTitle}>
+                      {new Date(
+                        session.forwardOrderDetails.created_at,
+                      ).toLocaleDateString()}
+                    </Text>
+                  </View>
+                  <View style={{ width: '50%' }}>
+                    <Text style={Styles.subText}>Total Product Price</Text>
+                    <Text style={Styles.subTitle}>
+                      Rs. {session.forwardOrderDetails.order_price}
+                    </Text>
+                  </View>
+                </View>
+                <View style={Styles.rowSection}>
+                  <View
+                    style={{
+                      ...Styles.rowSection,
+                      marginTop: 16,
+                      width: '90%',
+                    }}>
+                    <View style={{ width: '45%' }}>
+                      <Text style={Styles.subText}>SKU</Text>
+                      <Text style={Styles.subTitle}>
+                        {session.forwardOrderDetails.sku || '-'}
+                      </Text>
+                    </View>
+                    <View style={{ width: '45%' }}>
+                      <Text style={Styles.subText}>HSN</Text>
+                      <Text style={Styles.subTitle}>
+                        {session.forwardOrderDetails.hsn || '-'}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    ...Styles.rowSection,
+                    marginVertical: 16,
+                    width: '90%',
+                  }}>
+                  <View style={{ width: '45%' }}>
+                    <Text style={Styles.subText}>Unit Price</Text>
+                    <Text style={Styles.subTitle}>
+                      Rs. {session.forwardOrderDetails.product_price}
+                    </Text>
+                  </View>
+                  <View style={{ width: '45%' }}>
+                    <Text style={Styles.subText}>Quantity</Text>
+                    <Text style={Styles.subTitle}>
+                      {session.forwardOrderDetails.quantity}
+                    </Text>
+                  </View>
+                </View>
+                <View style={Styles.allButtonSection}>
+                  {buttons.map((obj, i) => {
+                    return (
+                      <View key={i} style={Styles.editButtonSection}>
+                        <Button
+                          text={obj.name}
+                          onPress={() => {
+                            handleUpdate(obj.post_value);
+                          }}
+                          color={obj.status ? 'white' : Constants.primaryColor}
+                          backgroundColor={
+                            obj.status ? Constants.primaryColor : 'white'
+                          }
+                          borderColor={Constants.primaryColor}
+                          borderWidth={1}
+                        />
+                      </View>
+                    );
+                  })}
+                </View>
+              </View>
             </View>
-          </View>
-        </View>
-      </ScrollView>
+          </ScrollView>
+        </>
+      )}
     </>
   );
 };
